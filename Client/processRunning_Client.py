974eb78af0883a4531ea92f48044ca2c0076a024
TonDucQuy <136158521+TonDucQuy@users.noreply.github.com>
2023-08-12 11:32:55 +0700

    Add note
    add note for more understanding

--- Client/processRunning_Client.py
+++ Client/processRunning_Client.py
@@ -6,82 +6,82 @@ from tkinter import Tk, Entry, Button, messagebox, END
 class ProcessWindow:       
     def __init__(self, client):         # Tạo cửa sổ GUI từ phương pháp _init_ và thiết lập cài đăt ban đầu 
         self.process = Tk()
-        self.process.title("Process Running")
-        self.process.configure(bg="#FFFAF0")
-        self.process.iconbitmap('./img/button/remoteIcon.ico')
+        self.process.title("Process Running") # Tựa đề
+        self.process.configure(bg="#FFFAF0") # Background
+        self.process.iconbitmap('./img/button/remoteIcon.ico') # Icon 
 
         self.client = client
         self.process_activity = None
 
-        self.create_widgets()  
+        self.create_widgets()  # Thực hiện hàm
 
     def create_widgets(self): # Hàm create_widgets tạo các button để thực hiện các chức năng bên dưới
-        start_button = Button(self.process, text="Start", font="Helvetica 10 bold", padx=30, pady=20,                   # Định dạng cho các button
-                              command=self.start_process, bd=5, bg="#000940", fg="#fff", activebackground='#fff')
-        start_button.grid(row=0, column=0, padx=8)
+        start_button = Button(self.process, text="Start", font="Helvetica 10 bold", padx=30, pady=20,         
+                              command=self.start_process, bd=5, bg="#000940", fg="#fff", activebackground='#fff')  # Định dạng button start
+        start_button.grid(row=0, column=0, padx=8) # Vi trí của button trong grid
 
-        watch_button = Button(self.process, text="Watch", font="Helvetica 10 bold", padx=30, pady=20,
-                              command=self.watch_processes, bd=5, bg="#000940", fg="#fff", activebackground='#fff')
-        watch_button.grid(row=0, column=1, padx=8)
+        watch_button = Button(self.process, text="Watch", font="Helvetica 10 bold", padx=30, pady=20,  
+                              command=self.watch_processes, bd=5, bg="#000940", fg="#fff", activebackground='#fff') # Định dạng button watch
+        watch_button.grid(row=0, column=1, padx=8) # Vị trí của button trong grid
 
         kill_button = Button(self.process, text="Kill", font="Helvetica 10 bold", padx=30, pady=20,
-                             command=self.kill_process, bd=5, bg="#000940", fg="#fff", activebackground='#fff')
-        kill_button.grid(row=0, column=2, padx=8)
+                             command=self.kill_process, bd=5, bg="#000940", fg="#fff", activebackground='#fff') # Định dạng button kill
+        kill_button.grid(row=0, column=2, padx=8)   # Vị trí của button trong grid
 
         delete_button = Button(self.process, text="Delete", font="Helvetica 10 bold", padx=30, pady=20,
-                               command=self.clear, bd=5, bg="#000940", fg="#fff", activebackground='#fff')
-        delete_button.grid(row=0, column=3, padx=8)
+                               command=self.clear, bd=5, bg="#000940", fg="#fff", activebackground='#fff') # Định dạng button delete
+        delete_button.grid(row=0, column=3, padx=8) # Vị trí của button trong grid
 
     def clear(self):     # Hàm clear để dọn dẹp các tiến trình hiện tại để thay các tiến trình khác
         if self.process_activity:
-            self.process_activity.destroy()
+            self.process_activity.destroy()  # Xóa các tiến trình hiện tại
 
     def watch_processes(self): # Hàm watch_processes dùng để xem các chương trình
         self.clear()
 
         try:
-            self.client.sendall(bytes("Watch_ProcessRunning", "utf-8"))
+            self.client.sendall(bytes("Watch_ProcessRunning", "utf-8"))  # Gửi lệnh Watch_ProcessRunning cho server
         except:
-            messagebox.showinfo("Error !!!", "Lỗi kết nối")
+            messagebox.showinfo("Error !!!", "Lỗi kết nối") # Thông báo lỗi
             self.process.destroy()
 
     def kill_process(self):     # Hàm kill_processes dùng để dừng các chương trình
         self.clear()                    # Tạo cửa sổ screen_KillTask khi button Kill được click
                                         # Yêu cầu nhập tên chương trình, 1 nút button dùng để xác nhận chương trình cần dừng 
         self.screen_KillTask = Tk()         
-        self.screen_KillTask.geometry("320x50")
-        self.screen_KillTask.title("Kill")
+        self.screen_KillTask.geometry("320x50") # Kích thước
+        self.screen_KillTask.title("Kill")  # Tên
 
-        self.Name_input = Entry(self.screen_KillTask, width=35)
-        self.Name_input.grid(row=0, column=0, columnspan=3, padx=5, pady=5)
-        self.Name_input.insert(END, "Nhập tên")
+        self.Name_input = Entry(self.screen_KillTask, width=35)   # Định dạng chỗ input
+        self.Name_input.grid(row=0, column=0, columnspan=3, padx=5, pady=5) # Grid của input
+        self.Name_input.insert(END, "Nhập tên") # Thêm "Nhập tên"
 
         Kill_Button = Button(self.screen_KillTask, bg="#000940", fg="#fff", text="Kill", font="Helvetica 10 bold",
-                             padx=20, command=self.kill_func, bd=5, activebackground='#7c6e6c')
-        Kill_Button.grid(row=0, column=4, padx=5, pady=5)
+                             padx=20, command=self.kill_func, bd=5, activebackground='#7c6e6c')  # Chức năng và định dạng cho button
+        Kill_Button.grid(row=0, column=4, padx=5, pady=5) # grid cho button
 
     def kill_func(self):        # Hàm kill_func thực hiện song song hàm kill_process, thực hiện để xác nhận Input cho vào
         self.AppName = self.Name_input.get()                # Nếu chương trình có tồn tại thì sẽ thực hiện kill_process vào thông báo thì kết thúc
         self.client.sendall(bytes("Kill_Task", "utf-8"))    # Ngược lại sẽ thông báo lỗi và không thực hiện hàm kill_process
         try:
-            self.client.sendall(bytes(self.AppName, "utf-8"))
-            self.checkdata = self.client.recv(1024).decode("utf-8")
-            messagebox.showinfo("", "Đã đóng chương trình")
+            self.client.sendall(bytes(self.AppName, "utf-8"))  # Gửi input cho server
+            self.checkdata = self.client.recv(1024).decode("utf-8") # Kiểm tra input 
+            messagebox.showinfo("", "Đã đóng chương trình")  # Thông báo 
         except:
-            messagebox.showinfo("Error !!!", "Không tìm thấy chương trình")
+            messagebox.showinfo("Error !!!", "Không tìm thấy chương trình") # Báo lỗi
 
     def start_process(self):    # Hàm start_process tương tự như kill_process nhưng là chạy chương trình thay vì dừng
         self.screen_Start = Tk()
-        self.screen_Start.geometry("320x50")
-        self.screen_Start.title("Start")
+        self.screen_Start.geometry("320x50") # Kích thước 
+        self.screen_Start.title("Start")    # Tên
 
-        self.Name_input = Entry(self.screen_Start, width=35)
-        self.Name_input.grid(row=0, column=0, columnspan=3, padx=5, pady=5)
-        self.Name_input.insert(END, "Nhập Tên")
+        self.Name_input = Entry(self.screen_Start, width=35) # Kích thước khung input
+        self.Name_input.grid(row=0, column=0, columnspan=3, padx=5, pady=5) # Grid của input
+        self.Name_input.insert(END, "Nhập Tên") # Thêm "Nhập tên"
 
         Start_Button = Button(self.screen_Start, text="Start", bg="#000940", fg="#fff", font="Helvetica 10 bold",
-                              padx=20, command=self.press_start, bd=5)
-        Start_Button.grid(row=0, column=4, padx=5, pady=5)
+                              padx=20, command=self.press_start, bd=5) # Chức năng và định dạng cho button
+        Start_Button.grid(row=0, column=4, padx=5, pady=5) # Grid cho button 
 
     def press_start(self):  # Hàm press_start tương tự kill_func , dùng để kiểm tra input nhập vào
         self.Name = self.Name_input.get()
--- Client/screenCapture_Client.py
+++ Client/screenCapture_Client.py
@@ -6,57 +6,57 @@ from tkinter import filedialog
 from PIL import ImageTk, Image
 
 
-class ScreenCapture:
+class ScreenCapture:   # Hàm dùng để chụp màn hình
     def __init__(self, client):
         self.client = client
-        self.Screenshot = Toplevel()
-        self.Screenshot.title("ScreenShot")
-        self.Screenshot.resizable(width=False, height=False)
-        self.Screenshot.iconbitmap('./img/button/remoteIcon.ico')
-        self.Screenshot.configure(bg="#C0C0C0", width=1090, height=610)
+        self.Screenshot = Toplevel()    # Tạo 1 cửa sổ
+        self.Screenshot.title("ScreenShot")  # Tên cửa sổ
+        self.Screenshot.resizable(width=False, height=False) # Ngăn việc chỉnh kích thước cửa sổ
+        self.Screenshot.iconbitmap('./img/button/remoteIcon.ico') # Icon button
+        self.Screenshot.configure(bg="#C0C0C0", width=1090, height=610) # Cài đặt kích thước cửa sổ
 
         self.canvas = Canvas(self.Screenshot, bg="white", width=1084, height=531)   # Tạo khung ảnh
-        self.canvas.place(relx=0, rely=0)   
+        self.canvas.place(relx=0, rely=0)   # Tọa độ khung ảnh (x=0,y=0)
 
-        self.btnChup = PhotoImage(file='./img/button/chup.png')
+        self.btnChup = PhotoImage(file='./img/button/chup.png') # Button cho chụp
         self.cap = Button(self.Screenshot, image=self.btnChup, command=self.receive_picture, bd=0) # Chụp ảnh
-        self.cap.place(relx=0, rely=0.88)
+        self.cap.place(relx=0, rely=0.88) # Tọa độ button
 
-        self.btnSave = PhotoImage(file='./img/button/luu.png')
+        self.btnSave = PhotoImage(file='./img/button/luu.png') # Button lưu ảnh
         self.Save = Button(self.Screenshot, image=self.btnSave, command=self.save_picture, bd=0) # Lưu ảnh
-        self.Save.place(relx=0.4532, rely=0.88)
+        self.Save.place(relx=0.4532, rely=0.88) # Tọa độ button 
 
-        self.btnDSave = PhotoImage(file='./img/button/khongluu.png')
+        self.btnDSave = PhotoImage(file='./img/button/khongluu.png') # Button không lưu ảnh
         self.DontSave = Button(self.Screenshot, image=self.btnDSave, command=self.dont_save_picture, bd=0) # Không lưu ảnh
-        self.DontSave.place(relx=0.7531, rely=0.88)
+        self.DontSave.place(relx=0.7531, rely=0.88) # Tọa độ button
 
-    def receive_picture(self):
+    def receive_picture(self):      # Hàm để chương trình nhận ảnh
         try:
             self.client.sendall(bytes("screenCapture", "utf-8")) # Gửi ảnh
         except:
             messagebox.showinfo("Error !!!", "Lỗi kết nối") # Thất bại báo lỗi
-            self.Screenshot.destroy()
+            self.Screenshot.destroy() # Hủy ảnh
 
-        self.file = open("picture.png", 'wb') # Mở ảnh để viết ?
-        self.data = self.client.recv(20126062)
-        self.file.write(self.data)
+        self.file = open("picture.png", 'wb') # Mở ảnh rỗng để viết byte
+        self.data = self.client.recv(20126062) # Truyền dữ liệu byte từ client theo socket 20126062
+        self.file.write(self.data) # Viết vào ảnh dữ liệu 
         self.img = ImageTk.PhotoImage(Image.open("picture.png")) # Mở ảnh để xem
-        self.canvas.create_image(0, 0, anchor=NW, image=self.img)
-        self.file.close()
+        self.canvas.create_image(0, 0, anchor=NW, image=self.img) # Toa độ ảnh
+        self.file.close() # Đóng ảnh để ngừng viết
 
-    def save_picture(self):
-        self.myScreenShot = open("picture.png", 'rb') # Mở ảnh để đọc
-        self.data = self.myScreenShot.read()
-        self.fname = filedialog.asksaveasfilename(title=u'Save file', filetypes=[("PNG", ".png")])
-        self.myScreenShot.close()
+    def save_picture(self): # Hàm để lưu ảnh
+        self.myScreenShot = open("picture.png", 'rb') # Mở ảnh để đọc binary
+        self.data = self.myScreenShot.read() # Lưu dữ liệu ảnh vào data
+        self.fname = filedialog.asksaveasfilename(title=u'Save file', filetypes=[("PNG", ".png")]) # Mở cửa sổ để chọn chỗ lưu ảnh và tên ảnh
+        self.myScreenShot.close() # Đóng ảnh để ngừng đọc
 
         self.file = open(str(self.fname) + '.png', 'wb') # Mở ảnh để viết
-        self.file.write(self.data)
-        self.file.close()
-        os.remove("picture.png") # Xóa ảnh trong bộ nhớ
-        self.Screenshot.destroy()
+        self.file.write(self.data) # Viết vào ảnh dữ liệu 
+        self.file.close() # Đóng ảnh để ngừng viết 
+        os.remove("picture.png") # Xóa ảnh trong bộ nhớ (bộ nhớ tạm thời) 
+        self.Screenshot.destroy() # Đóng cửa sổ lưu ảnh
 
-    def dont_save_picture(self):
-        os.remove("picture.png")
-        self.Screenshot.destroy()
+    def dont_save_picture(self):  # Hàm để xóa ảnh
+        os.remove("picture.png") # Xóa ảnh trong bộ nhớ (bộ nhớ tạm thời)
+        self.Screenshot.destroy() # Đóng cửa sổ lưu ảnh
 
